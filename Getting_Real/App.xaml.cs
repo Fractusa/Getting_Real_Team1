2381d59d746c9831d2074e2857ff1be8fdb2cb62
André <ehris12@hotmail.com>
2024-05-17 15:07:39 +0200

    Initial project created

---
+++ Getting_Real/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Getting_Real
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
